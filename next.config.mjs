d51e61758374412410aa6a1d5e8caa7c7a866877
hienvv17 <hienvv17@gmail.com>
2025-05-10 00:14:20 +0700

    fix-avatar

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // images: {
-    //     domains: ["lh3.googleusercontent.com", "res.cloudinary.com", "ik.imagekit.io"],
-    // },
+    images: {
+        domains: ["lh3.googleusercontent.com", "res.cloudinary.com",],
+    },
     async rewrites() {
         return [
             { source: "/dat-san/:path*", destination: "/sports-field-booking/:path*" },
